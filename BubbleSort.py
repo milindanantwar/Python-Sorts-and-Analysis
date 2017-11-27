a10f71eba1fa79131109e50f1ee55ecf82a14394
milindanantwar <milind.encognatia@gmail.com>
2017-11-27 09:48:42 +0530

    added bubble sort code

---
+++ BubbleSort.py
@@ -0,0 +1,24 @@
+# Bubble Sort 
+#    _____  .__.__  .__            .___ _____
+#   /     \ |__|  | |__| ____    __| _//  _  \
+#  /  \ /  \|  |  | |  |/    \  / __ |/  /_\  \
+# /    Y    \  |  |_|  |   |  \/ /_/ /    |    \
+# \____|__  /__|____/__|___|  /\____ \____|__  /
+#         \/                \/      \/       \/
+
+import itertools;
+
+#listCount = input("Enter list size: \n");
+listFromUser = input("Enter list elements separated by space: \n");
+listArrayStr = listFromUser.split();
+listArray =  [int(x) for x in listArrayStr];
+listLength = len(listArray)
+for idx,valx in enumerate(listArray):
+    for idy,ele in enumerate(listArray):
+        if idy < listLength-1 and listArray[idy] > listArray[idy+1]:
+            temp = listArray[idy]
+            listArray[idy] = listArray[idy+1]
+            listArray[idy+1] = temp
+
+            
+print(listArray)
