8e88cea89d2499686b91e8ebba8a23e963f132a7
milindanantwar <milind.encognatia@gmail.com>
2017-11-27 10:06:43 +0530

    added factorial of a number

---
+++ Factorial.py
@@ -0,0 +1,20 @@
+# Factoarial of a number
+#    _____  .__.__  .__            .___ _____
+#   /     \ |__|  | |__| ____    __| _//  _  \
+#  /  \ /  \|  |  | |  |/    \  / __ |/  /_\  \
+# /    Y    \  |  |_|  |   |  \/ /_/ /    |    \
+# \____|__  /__|____/__|___|  /\____ \____|__  /
+#         \/                \/      \/       \/
+
+
+def factorial(num):
+    if num == 0:
+        return 1;
+    else:
+        return num * factorial(num-1);
+
+
+numFromUser = input("Enter element for factorial: \n");
+numForFactorial = int(numFromUser);
+
+print("Factorial of number is : "+str(factorial(numForFactorial)));
